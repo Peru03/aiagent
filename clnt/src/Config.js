0ffe4d80a1f96440ab5d6f262b3786b6f777f95c
Prerna <prerna.janbandhu18@gmail.com>
2025-09-27 11:14:53 +0530

    added static cors headers

--- clnt/src/Config.js
+++ clnt/src/Config.js
@@ -10,6 +10,12 @@ API.interceptors.request.use((config) => {
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
+  // CORS headers (informational only; backend must allow them)
+  config.headers["Access-Control-Allow-Origin"] = "*";
+  config.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS";
+  config.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization";
+
   return config;
 }, (error) => {
   return Promise.reject(error);
